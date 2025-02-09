8bacf63f41bba4b55b9edd3b58ef905a150fe5fd
ycsu <hchs810751@gmail.com>
2025-02-09 18:06:58 +0800

    Remove Legacy

--- pyTableGen/cov_test.sh
+++ pyTableGen/cov_test.sh
@@ -1,4 +1,6 @@
 mkdir -p cov_data
-LLVM_PROFILE_FILE="cov_data/cov.profraw" python3 -m pytest --cov-report=lcov:cov_data/cov.info --cov=src 
+LLVM_PROFILE_FILE="cov_data/cov.profraw" python3 -m pytest --cov-report=term-missing --cov-report=lcov:cov_data/cov.info --cov=src 
 llvm-profdata merge -sparse cov_data/cov.profraw -o cov_data/cov.profdata
+echo "Show Coverage of Binding part"
+sh cov_show.sh
 
